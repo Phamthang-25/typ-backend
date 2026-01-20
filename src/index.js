37ec3aa10a0345fb466669576d39fe35b413558d
Phamthang <gm.gmphamthang@gmail.com>
2026-01-21 00:31:35 +0700

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { createPoolFromEnv } = require("./db");
-
+// helo
 const app = express();
 app.use(cors());
 app.use(express.json());
